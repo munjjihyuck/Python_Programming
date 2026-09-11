e475ae6e9b91ca6c60e591587f9ca257446ce59f
munjjihuck <munjjihyuck@dimigo.hs.kr>
2026-09-11 14:18:50 +0900

    collection. 9/11

--- 04_collection/ex4.py
+++ 04_collection/ex4.py
@@ -138,3 +138,12 @@ total = sum(price * qty for _, price, qty in stocks)
 
 # ✅ 총액: 21,000원
 print(f"총액: {total:,}원")
+
+stocks = (
+    ("사과", "바나나", "체리"),
+    (1000, 2000, 5000),
+    (5, 3, 2),
+)
+
+total = sum(price * qty for _, price, qty in zip(*stocks))
+print(f"총액: {total:,}원")
